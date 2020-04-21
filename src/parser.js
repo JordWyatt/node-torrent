d67ca5e5e183d36e525fa2cf64ef1dd354021d2f
jwyatt1995@gmail.com <jwyatt1995@gmail.com>
2020-04-21 23:23:44 +0100

    Add parser functions for piece length

--- src/parser.js
+++ src/parser.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const bencode = require("bencode");
 const crypto = require("crypto");
 
+const BLOCK_LENGTH = Math.pow(2, 14);
+
 const open = (path) => {
   if (fs.existsSync(path)) {
     const buffer = fs.readFileSync(path);
@@ -22,9 +24,28 @@ const getPeerId = () => "-qB4210-PH.d7a-hn83h";
 
 const getLength = (torrent) => torrent.info.length;
 
+const getPieceLength = (torrent, pieceIndex) => {
+  const maxPieceLength = torrent.info.piece_length;
+  const fileLength = torrent.info.length;
+
+  const lastPieceLength = fileLength % maxPieceLength;
+
+  const start = pieceIndex * maxPieceLength;
+  const end = start + maxPieceLength;
+
+  return end > lastPieceLength ? lastPieceLength : maxPieceLength;
+};
+
+const getBlocksPerPiece = (torrent, pieceIndex) => {
+  const pieceLength = this.getPieceLength(torrent, pieceIndex);
+  return Math.ceil(pieceLength / BLOCK_LENGTH);
+};
+
 module.exports = {
   open,
   getInfoHash,
   getPeerId,
   getLength,
+  getBlocksPerPiece,
+  getPieceLength,
 };
